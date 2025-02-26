0439f48fa2b8d010a097227663d937919190711f
Conor Durcan <conordurcan13@gmail.com>
2025-02-26 11:32:03 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'data-binding-app';
+  count:number = 0;
+  show:boolean = true;
+
+  onButtonClick()
+  {
+    this.count++;
+  }
+
+  onstarDBLClick(){
+    this.show = !this.show;
+  }
 }
